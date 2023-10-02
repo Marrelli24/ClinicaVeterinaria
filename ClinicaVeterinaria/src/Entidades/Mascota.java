d2e35168c71228a1aa21865f8402c5756e9e4dec
Ema-Schonfeld <emaschonfeld180603@gmail.com>
2023-10-02 12:21:26 -0300

    Mascota
    Saque estado de los atributos de mascota

--- ClinicaVeterinaria/src/Entidades/Mascota.java
+++ ClinicaVeterinaria/src/Entidades/Mascota.java
@@ -1,13 +1,111 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Entidades;
 
-/**
- *
- * @author 54343
- */
+import java.time.LocalDate;
+
+
 public class Mascota {
+    
+    private int idMascota;
+    private String alias;
+    private String sexo;
+    private String especie;
+    private String raza;
+    private String colorPelo;
+    private double pesoPromedio;
+    private LocalDate fechaNac;
+
+    public Mascota() {
+    }
+
+    public Mascota(String alias, String sexo, String especie, String raza, String colorPelo, double pesoPromedio, LocalDate fechaNac) {
+        this.alias = alias;
+        this.sexo = sexo;
+        this.especie = especie;
+        this.raza = raza;
+        this.colorPelo = colorPelo;
+        this.pesoPromedio = pesoPromedio;
+        this.fechaNac = fechaNac;
+    }
+
+    public Mascota(int idMascota, String alias, String sexo, String especie, String raza, String colorPelo, double pesoPromedio, LocalDate fechaNac) {
+        this.idMascota = idMascota;
+        this.alias = alias;
+        this.sexo = sexo;
+        this.especie = especie;
+        this.raza = raza;
+        this.colorPelo = colorPelo;
+        this.pesoPromedio = pesoPromedio;
+        this.fechaNac = fechaNac;
+    }
+
+    public int getIdMascota() {
+        return idMascota;
+    }
+
+    public void setIdMascota(int idMascota) {
+        this.idMascota = idMascota;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public String getColorPelo() {
+        return colorPelo;
+    }
+
+    public void setColorPelo(String colorPelo) {
+        this.colorPelo = colorPelo;
+    }
+
+    public double getPesoPromedio() {
+        return pesoPromedio;
+    }
+
+    public void setPesoPromedio(double pesoPromedio) {
+        this.pesoPromedio = pesoPromedio;
+    }
+
+    public LocalDate getFechaNac() {
+        return fechaNac;
+    }
+
+    public void setFechaNac(LocalDate fechaNac) {
+        this.fechaNac = fechaNac;
+    }
+
+    @Override
+    public String toString() {
+        return "Mascota{" + "idMascota=" + idMascota + ", alias=" + alias + ", sexo=" + sexo + ", especie=" + especie + ", raza=" + raza + ", colorPelo=" + colorPelo + ", pesoPromedio=" + pesoPromedio + ", fechaNac=" + fechaNac + '}';
+    }
     
 }

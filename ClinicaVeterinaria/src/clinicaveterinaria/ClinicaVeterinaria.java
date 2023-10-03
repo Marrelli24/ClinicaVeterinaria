c6d8ea4599057dc5e59512b533ff8ab8a97d7408
Marrelli24 <marrellirt17@gmail.com>
2023-10-03 12:13:56 -0300

    Cliente
    Correcciones en clienteData

--- ClinicaVeterinaria/src/clinicaveterinaria/ClinicaVeterinaria.java
+++ ClinicaVeterinaria/src/clinicaveterinaria/ClinicaVeterinaria.java
@@ -4,19 +4,23 @@ import AccesoADatos.ClienteData;
 import Entidades.Cliente;
 import AccesoADatos.TratamientoData;
 
-
 public class ClinicaVeterinaria {
 
     public static void main(String[] args) {
-       
-        
+
         //Pruebas de Tratamiento
         TratamientoData trat = new TratamientoData();
-        
+
         System.out.println(trat.buscarTratamiento(1));
-        
-        ClienteData clientedata= new ClienteData();
+
+        ClienteData clientedata = new ClienteData();
         Cliente cliente = new Cliente(38299859, "Ezequiel", "Marrelli", 44, "Calle solari", 1);
+        clientedata.guardarCliente(cliente);
+        System.out.println(clientedata.buscarClientePorDni(cliente.getDni()));
+        System.out.println(clientedata.buscarClientePorId(cliente.getIdCliente()));
+        clientedata.editarCliente(cliente);
+        clientedata.eliminarCliente(cliente.getIdCliente());
+
     }
-    
+
 }

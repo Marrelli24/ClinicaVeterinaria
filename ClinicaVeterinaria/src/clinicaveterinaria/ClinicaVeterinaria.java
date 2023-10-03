3b6358e40ba606d8f814305a04129f262c01297f
Marrelli24 <marrellirt17@gmail.com>
2023-10-03 10:36:31 -0300

    prueba2

--- ClinicaVeterinaria/src/clinicaveterinaria/ClinicaVeterinaria.java
+++ ClinicaVeterinaria/src/clinicaveterinaria/ClinicaVeterinaria.java
@@ -1,17 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package clinicaveterinaria;
 
 import AccesoADatos.ClienteData;
 import Entidades.Cliente;
+import AccesoADatos.TratamientoData;
 
-/**
- *
- * @author Ezequiel
- */
+
 public class ClinicaVeterinaria {
 
     public static void main(String[] args) {

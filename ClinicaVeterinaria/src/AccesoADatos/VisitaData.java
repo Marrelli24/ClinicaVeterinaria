d7eb926436b4b7d51e2b6a704140cb9bc69241fe
Marrelli24 <marrellirt17@gmail.com>
2023-10-03 12:29:49 -0300

    Visita
    nombres de los metodos

--- ClinicaVeterinaria/src/AccesoADatos/VisitaData.java
+++ ClinicaVeterinaria/src/AccesoADatos/VisitaData.java
@@ -1,6 +1,8 @@
 package AccesoADatos;
 
+import Entidades.Visita;
 import java.sql.Connection;
+import java.util.ArrayList;
 
 public class VisitaData {
 
@@ -16,4 +18,38 @@ public class VisitaData {
         tratamientoData = new TratamientoData();
     }
 
+    public void guardarVisita(Visita visita) {
+    }
+
+    public void editarVisita(Visita visita) {
+    }
+
+    public void eliminarVisita(int id) {
+    }
+
+    public ArrayList<Visita> listarVisita() {
+        ArrayList<Visita> lista = new ArrayList<>();
+        Visita visita = null;
+
+        return lista;
+    }
+  
+     public ArrayList<Visita> buscarVisitaPorId() {
+        ArrayList<Visita> lista = new ArrayList<>();
+        Visita visita = null;
+
+        return lista;
+    }    
+     public ArrayList<Visita> buscarVisitaPorCliente() {
+        ArrayList<Visita> lista = new ArrayList<>();
+        Visita visita = null;
+
+        return lista;
+    }   
+      public ArrayList<Visita> buscarVisitaPorMascota() {
+        ArrayList<Visita> lista = new ArrayList<>();
+        Visita visita = null;
+
+        return lista;
+    }   
 }

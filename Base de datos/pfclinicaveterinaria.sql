6f5228716338b777c0c764f895501fdabb6debe6
Marrelli24 <marrellirt17@gmail.com>
2023-10-04 12:14:11 -0300

    Create pfclinicaveterinaria.sql

---
+++ Base de datos/pfclinicaveterinaria.sql
@@ -0,0 +1,215 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 04-10-2023 a las 17:12:45
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `pfclinicaveterinaria`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `idCliente` int(11) NOT NULL,
+  `dni` int(11) NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  `apellido` varchar(30) NOT NULL,
+  `telefono` bigint(20) NOT NULL,
+  `direccion` varchar(50) NOT NULL,
+  `contactoAlternativo` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `cliente`
+--
+
+INSERT INTO `cliente` (`idCliente`, `dni`, `nombre`, `apellido`, `telefono`, `direccion`, `contactoAlternativo`) VALUES
+(1, 30100100, 'Mariano', 'Alvarez', 154100010, 'Av Sarmiento', 155202020),
+(2, 30100100, 'Mariano', 'Alvarez', 154100010, 'Av Sarmiento', 155202020),
+(3, 40123456, 'Juan', 'Martinez', 154999999, 'Av San Martin', 154888888),
+(4, 40123456, 'Juan', 'Martinez', 154999999, 'Av San Martin', 154888888),
+(5, 28888999, 'Julia', 'Marquez', 154222333, 'Av Belgrano', 154100200),
+(6, 28888999, 'Julia', 'Marquez', 154222333, 'Av Belgrano', 154100200),
+(7, 19100100, 'Pedro', 'Lopez', 154151515, 'nose', 154987654),
+(8, 19100100, 'Pedro', 'Lopez', 154151515, 'nose', 154987654),
+(9, 26000000, 'Juan', 'Alvarez', 11111111, 'Av Sarmiento', 2222222),
+(10, 1123234, 'Julia', 'Alvarez', 11111111, 'Av San Martin', 2222222);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mascota`
+--
+
+CREATE TABLE `mascota` (
+  `idMascota` int(11) NOT NULL,
+  `alias` varchar(30) NOT NULL,
+  `sexo` varchar(30) NOT NULL,
+  `especie` varchar(30) NOT NULL,
+  `raza` varchar(30) NOT NULL,
+  `colorPelo` varchar(50) NOT NULL,
+  `peso` double(6,3) NOT NULL,
+  `pesoPromedio` double(6,3) NOT NULL,
+  `fechaNac` date NOT NULL,
+  `idCliente` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `mascota`
+--
+
+INSERT INTO `mascota` (`idMascota`, `alias`, `sexo`, `especie`, `raza`, `colorPelo`, `peso`, `pesoPromedio`, `fechaNac`, `idCliente`) VALUES
+(1, 'Indiana', 'Femenino', 'Canino', 'Caniche', 'Blanco', 8.000, 7.000, '2016-06-15', 1),
+(2, 'Catalina', 'Femenino', 'Felino', 'Calle', 'Naranja blanco', 6.000, 0.000, '2017-08-16', 4),
+(3, 'Leo', 'Masculino', 'Canino', 'Salchicha', 'Marron', 0.000, 0.000, '2018-08-16', 5),
+(4, 'Messi', 'Masculino', 'Canino', 'Ovejero Aleman', 'Negro', 0.000, 0.000, '2022-12-18', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tratamiento`
+--
+
+CREATE TABLE `tratamiento` (
+  `idTratamiento` int(11) NOT NULL,
+  `descripcion` varchar(70) NOT NULL,
+  `medicamento` varchar(50) NOT NULL,
+  `importe` double NOT NULL,
+  `tipodeTratamiento` varchar(30) NOT NULL,
+  `activo` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `tratamiento`
+--
+
+INSERT INTO `tratamiento` (`idTratamiento`, `descripcion`, `medicamento`, `importe`, `tipodeTratamiento`, `activo`) VALUES
+(1, 'Baño', 'Ninguno', 5000, 'Estetica', 1),
+(2, 'Desparacitar', 'Antiparacitario', 7000, 'Control', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `visita`
+--
+
+CREATE TABLE `visita` (
+  `idVisita` int(11) NOT NULL,
+  `idMascota` int(11) NOT NULL,
+  `fechaVisita` date NOT NULL,
+  `detalle` varchar(50) NOT NULL,
+  `peso` double(6,3) NOT NULL,
+  `idTratamiento` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `visita`
+--
+
+INSERT INTO `visita` (`idVisita`, `idMascota`, `fechaVisita`, `detalle`, `peso`, `idTratamiento`) VALUES
+(1, 1, '2023-08-16', 'Baño', 8.000, 1),
+(2, 2, '2023-06-25', 'Desparacitar', 6.000, 2),
+(3, 3, '2023-06-25', 'Baño', 4.000, 2),
+(4, 4, '2023-08-16', 'Baño', 15.000, 1),
+(5, 1, '2023-09-20', 'Baño', 6.000, 1),
+(6, 1, '2023-09-25', 'Desparacitar', 7.000, 2),
+(7, 1, '2014-05-13', 'Baño', 500.000, 1);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`idCliente`);
+
+--
+-- Indices de la tabla `mascota`
+--
+ALTER TABLE `mascota`
+  ADD PRIMARY KEY (`idMascota`),
+  ADD KEY `idCliente` (`idCliente`);
+
+--
+-- Indices de la tabla `tratamiento`
+--
+ALTER TABLE `tratamiento`
+  ADD PRIMARY KEY (`idTratamiento`);
+
+--
+-- Indices de la tabla `visita`
+--
+ALTER TABLE `visita`
+  ADD PRIMARY KEY (`idVisita`),
+  ADD KEY `idMascota` (`idMascota`),
+  ADD KEY `idTratamiento` (`idTratamiento`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `idCliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT de la tabla `mascota`
+--
+ALTER TABLE `mascota`
+  MODIFY `idMascota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `tratamiento`
+--
+ALTER TABLE `tratamiento`
+  MODIFY `idTratamiento` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `visita`
+--
+ALTER TABLE `visita`
+  MODIFY `idVisita` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `mascota`
+--
+ALTER TABLE `mascota`
+  ADD CONSTRAINT `mascota_ibfk_1` FOREIGN KEY (`idCliente`) REFERENCES `cliente` (`idCliente`);
+
+--
+-- Filtros para la tabla `visita`
+--
+ALTER TABLE `visita`
+  ADD CONSTRAINT `visita_ibfk_1` FOREIGN KEY (`idMascota`) REFERENCES `mascota` (`idMascota`),
+  ADD CONSTRAINT `visita_ibfk_2` FOREIGN KEY (`idTratamiento`) REFERENCES `tratamiento` (`idTratamiento`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

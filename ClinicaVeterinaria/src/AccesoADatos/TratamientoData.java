ccacf2ab05be6e40409299003c0c45ee6ca6f89c
JonBalles <Jonatan.a.Ballestero@gmail.com>
2023-10-06 10:41:34 -0300

    Cambios

--- ClinicaVeterinaria/src/AccesoADatos/TratamientoData.java
+++ ClinicaVeterinaria/src/AccesoADatos/TratamientoData.java
@@ -1,14 +1,11 @@
 package AccesoADatos;
 
-import Entidades.Cliente;
 import Entidades.Tratamiento;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 
